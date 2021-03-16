72cae1cc204cc8f9b298246e09babe5a7e9ccf5c
Ty Foster <tyfoster@outlook.com>
2021-03-15 17:25:45 -0700

    Add stubs, exports, and imports

---
+++ tafoste4_lab1/lab1act2partC.js
@@ -0,0 +1,17 @@
+/**
+ * @author Ty Foster
+ * @version 2021.03.15
+ * SER 421, Arizona State University
+ * Copyright 2021, all rights reserved.
+ * 
+ */
+
+class Calc {
+  //TODO
+};
+
+function exec(arr) {
+  //TODO
+};
+
+module.exports = { Calc, exec };
---
+++ tafoste4_lab1/lab1act2partC_test.js
@@ -0,0 +1,2 @@
+const { Calc, exec } = require('./lab1act2partC');
+//TODO
