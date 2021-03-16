2b48e2b22eae180298bc8c40a1c655f37702e87d
Ty Foster <tyfoster@outlook.com>
2021-03-15 17:01:41 -0700

    Reduces runtime

--- tafoste4_lab1/lab1act2partA_test.js
+++ tafoste4_lab1/lab1act2partA_test.js
@@ -2,12 +2,9 @@ const { Calc, exec } =  require('./lab1act2partA');
 console.log('TESTING calc()');
 var c = new Calc();
 
-console.log(c.calc('{"op": "add", "number": 0}')); // expected: 0
-console.log(c.calc('{"op": "add", "number": -1}')); // expected: -1
-console.log(c.calc('{"op": "subtract", "number": -1}')); // expected: 0
 console.log(c.calc('{"op": "add", "number": 5}')); // expected: 5
-console.log(c.calc('{"op": "subtract", "number": 10}')); // expected: -5
-console.log(c.calc('{"op": "add", "number": 15}')); // expected: 10
+console.log(c.calc('{"op": "subtract", "number": 2}')); // expected: 3
+console.log(c.calc('{"op": "add", "number": 19}')); // expected: 22
 
 console.log('\n\n\n\n');
 console.log('TESTING exec()');
