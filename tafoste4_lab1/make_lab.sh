1a040639e911d4f0cba46ee614015f972d07d63c
Ty Foster <tyfoster@outlook.com>
2021-03-25 11:42:33 -0700

    Rename file

---
+++ tafoste4_lab1/make_lab.sh
@@ -0,0 +1,3 @@
+pushd ~/git/tafoste4_lab1 # get to necessary folder
+zip ../tafoste4_lab1.zip README.txt lab1act1.html lab1act2partA.js lab1act2partB.js lab1act2partC.js lab1act2partA_test.js lab1act2partB_test.js lab1act2partC_test.js util.js # compress the files
+pushd -0 # navigate back to original directory
