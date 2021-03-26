aa2d984403973abc573dead25ee55fa7884c7eb6
Ty Foster <tyfoster@outlook.com>
2021-03-25 21:59:56 -0700

    Refactor utility functions

--- tafoste4_lab2/make_lab.sh
+++ tafoste4_lab2/make_lab.sh
@@ -1,3 +1,3 @@
 pushd ~/git/421/tafoste4_lab2 # navigates to lab directory from any folder
-zip ../tafoste4_lab2.zip README.txt util.js FAQ00.js FAQ00.test.js FAQService00.js FAQService00.test.js data/* forms/* # compresses files
+zip ../tafoste4_lab2.zip README.txt FAQ00.js FAQ00.test.js FAQService00.js FAQService00.test.js constants.js QA.json data/* forms/* util/* # compresses files
 pushd -0 # returns to original directory
