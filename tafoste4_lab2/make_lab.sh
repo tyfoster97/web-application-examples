8a10328644b4c65dff680e2352abbc9d15469dae
Ty Foster <tyfoster@outlook.com>
2021-03-25 18:51:56 -0700

    Updates zip

--- tafoste4_lab2/make_lab.sh
+++ tafoste4_lab2/make_lab.sh
@@ -1,3 +1,3 @@
 pushd ~/git/421/tafoste4_lab2 # navigates to lab directory from any folder
-zip ../tafoste4_lab2.zip README.txt util.js QA.json FAQ00.js FAQ00.test.js FAQService00.js FAQService00.test.js # compresses files
+zip ../tafoste4_lab2.zip README.txt util.js FAQ00.js FAQ00.test.js FAQService00.js FAQService00.test.js data/* forms/* # compresses files
 pushd -0 # returns to original directory
