2230481a6bb8815d4c58b376bc59bf3a7a70b9d5
Ty Foster <tyfoster@outlook.com>
2021-03-25 22:00:23 -0700

    Refactors outline

--- tafoste4_lab2/FAQService00.js
+++ tafoste4_lab2/FAQService00.js
@@ -1,12 +1,8 @@
 const http = require('http');
-const util = require('./util');
 const qstring = require('querystring');
-const fs = require('fs');
-const { LOGIN_SUCCESSFUL, USER_NOT_FOUND, PASSWORD_MISMATCH, 
-  TYPE_MISMATCH, LOGINFORM_A , LOGINFORM_B 
-} = require('./constants');
+const { LOGIN_SUCCESSFUL, USER_NOT_FOUND, PASSWORD_MISMATCH, TYPE_MISMATCH } = require('./constants');
+const { mapCookies, buildLoginForm, login } = require('./util/server');
 /* for simplicity */
-const pgHead = '<html><body>';
 const pgFoot = '</html></body>';
 /**
  * @author Ty Foster
@@ -21,23 +17,60 @@ const loginA =
 http.createServer(function (req, res) {
   // get cookies from header
   let cookies = req.headers.cookie.split(/;\s+/);
-  let cookieMap = util.mapCookies(cookies);
+  let cookieMap = mapCookies(cookies);
   if (req.method == 'GET') {
     res.writeHead(200);
     let resBody = '';
+    // TODO display welcome message if uname cookie is detected -> use helper method to build into resBody
     if (cookieMap.has('login') && cookieMap.get('login') == 'true') {
-      // TODO: display Q&A page
+      // TODO display Q&A page
+      resBody += 'hello';
     } else {
       if (cookieMap.has('uname')) {
-        resBody += util.buildLoginForm(cookieMap.get('uname'));
+        // TODO generate page header with welcome message
+        resBody += buildLoginForm(cookieMap.get('uname'));
       } else {
-        resBody += util.buildLoginForm('');
+        resBody += buildLoginForm('');
       }
     }
-    res.end(pgHead + resBody + pgFoot);
+    res.end(resBody + pgFoot);
   } else if (req.method == 'POST') {
-    // TODO: get postParams
-
-    // TODO: determine if login POST or database POST
+    // read in request
+    let reqData = '';
+    req.on('data', (chunk) => {
+      reqData += chunk;
+    });
+    req.on('end', () => {
+      let params = qstring.parse(reqData);
+      // TODO determine page calling POST
+      if (params.username && params.password) {
+        // TODO verify login
+        switch(login(params.username, params.password, params.usertype)) {
+          case LOGIN_SUCCESSFUL:
+            // TODO handle successful login
+            res.writeHead(200, [
+              ['Set-Cookie', 'uname=' + params.username],
+              ['Set-Cookie', 'login=true'],
+              ['Set-Cookie', 'utype=' + params.usertype]
+            ]);
+            res.end(util.pgHead(params.username, params.usertype, false) + 'logged in' + pgFoot);
+            break;
+          case USER_NOT_FOUND:
+            // TODO
+            res.setHeader(401);
+            break;
+          case PASSWORD_MISMATCH:
+            // TODO
+            break;
+          case TYPE_MISMATCH:
+            // TODO
+            break;
+          default:
+            res.setHeader(500); // Something has to go very wrong for this to happen
+            break;
+        }
+        // TODO send response
+      } // TODO handle other pages
+    });
   }
 }).listen(3000);
