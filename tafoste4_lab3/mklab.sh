ab0edd7befad5f7b8f206144e2be7d275b80b802
Ty Foster <tyfoster@outlook.com>
2021-04-03 17:19:49 -0700

    Modifies outfile name

--- tafoste4_lab3/mklab.sh
+++ tafoste4_lab3/mklab.sh
@@ -1,3 +1,3 @@
 pushd ~/git/421/tafoste4_lab3
-zip ../tafoste3_lab3.zip README.md activity1/bin/* activity1/data/* activity1/public/* activity1/routes/* activity1/__test__/* activity1/util/* activity1/views/* 
+zip ../tafoste4_lab3.zip README.md activity1/bin/* activity1/data/* activity1/public/* activity1/routes/* activity1/__test__/* activity1/util/* activity1/views/* 
 pushd -0
