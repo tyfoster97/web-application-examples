a9e26912f6c241d20f7308ffd8aa4ee6f69725f5
Ty Foster <tyfoster@outlook.com>
2021-04-08 18:01:58 -0700

    Initial commit

---
+++ tafoste4_lab4/activity1/activity1.js
@@ -0,0 +1,34 @@
+
+/**
+ * @file activity1.js
+ * @version 2021.04.08
+ * @author Ty Foster
+ * SER 421, Arizona Stae University
+ * Copyright 2021, All rights reserved.
+ * 
+ * Demonstrates DOM manipulation with JavaScript
+ */
+
+/**
+ * @task Output to the console the <ol> element encompassing the results of the search
+ */
+
+/**
+ * @task Output to the console the code for the "onload" event on the <body> element
+ */
+
+/**
+ * @task Output to the console the second child node underneath the <body> element
+ */
+
+/**
+ * @task Output to the console the number of <h2> tags in the page
+ */
+
+/**
+ * @task Output to the console the value in the search bar (must be from search bar)
+ */
+
+/**
+ * @task Make the "Add Bing New Tab Extension" text in the upper right corner result go away
+ */
