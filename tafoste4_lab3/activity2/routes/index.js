f872c939d9a0b138b43d15e3278bc5924af9c919
Ty Foster <tyfoster@outlook.com>
2021-04-08 17:49:54 -0700

    Lab 3 submission

--- tafoste4_lab3/activity2/routes/index.js
+++ tafoste4_lab3/activity2/routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.set('Cache-Control', 'no-cache, private, no-store, must-revalidate');
+  res.render('index', { title: 'Roomate Finder' });
 });
 
 module.exports = router;
