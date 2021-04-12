de75201c60cbebb117fdbf3a136bf0684ecb8f7d
Ty Foster <tyfoster@outlook.com>
2021-04-11 19:00:22 -0700

    Lab submission

--- tafoste4_lab4/mklab.sh
+++ tafoste4_lab4/mklab.sh
@@ -1,3 +1,3 @@
 pushd ~/git/421/tafoste4_lab4
-zip ../tafoste4_lab4 activity1/* activity2/activity2.js activity2/dictionary-1.js activity2/utils/* activity2/index.html
+zip ../tafoste4_lab4 activity1/* activity2/activity2.js activity2/utils/* activity2/index.html activity3/index.html activity3/utils/* activity3/activity3.js
 pushd -0
