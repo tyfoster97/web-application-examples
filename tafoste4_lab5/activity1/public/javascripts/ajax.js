b3a8d7fff2d108180fbe5e20ab92ab28fa9a7a38
Ty Foster <tyfoster@outlook.com>
2021-04-15 18:44:39 -0700

    Rename api.js to ajax.js

---
+++ tafoste4_lab5/activity1/public/javascripts/ajax.js
@@ -0,0 +1,50 @@
+/*******************************************************************************
+ * @file ape.js
+ * @version 2021.04.15
+ * @author Ty Foster
+ * SER 421, Arizona State University
+ * Copyright 2021, All rights reserved.
+ * 
+ * This file contains AJAX API methods for calling server endpoints
+ */
+
+const currencies = {
+  EURO: 'EU',
+  POUND: 'UK'
+};
+
+const conversions = {
+  EURO: 0.9,
+  POUND: 0.78
+};
+
+/***********************************************************
+ * Handles a Currency Conversion request from the Single 
+ * Page Application
+ * 
+ * @param {string} address the address for the HTTP Request
+ * @param {string} currency the result currency
+ */
+function ajaxConversionRequest(address, currency) {
+  //TODO
+}
+
+/***********************************************************
+ * Handles an AJAX Reuqest to the Server
+ * 
+ * @param {string} address the address for the HTTP request
+ */
+function ajaxRequest(address) {
+  //TODO
+}
+
+
+/***********************************************************
+ * Obtains an XML HTTP Request Object
+ * 
+ * @returns the XMLHttpRequest Object
+ */
+function _getRequest() {
+  //TODO
+}
+
