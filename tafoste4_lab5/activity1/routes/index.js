12c7bdc2846553cc5a3438fa766d23b3c1488a70
Ty Foster <tyfoster@outlook.com>
2021-04-15 19:18:10 -0700

    Adds default value for currency_type

--- tafoste4_lab5/activity1/routes/index.js
+++ tafoste4_lab5/activity1/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Currency Converter', amount: 0});
+  res.render('index', { title: 'Currency Converter', amount: 0, currency_type: 'USD'});
 });
 
 module.exports = router;
