e9aea649753fb13251bd2694aef6e0122b4b9b78
Ty Foster <tyfoster@outlook.com>
2021-04-15 19:31:00 -0700

    Adds method stubs

--- tafoste4_lab5/activity1/public/javascripts/ajax.js
+++ tafoste4_lab5/activity1/public/javascripts/ajax.js
@@ -1,40 +1,44 @@
 /*******************************************************************************
- * @file ape.js
+ * @file ajax.js
  * @version 2021.04.15
  * @author Ty Foster
  * SER 421, Arizona State University
  * Copyright 2021, All rights reserved.
  * 
- * This file contains AJAX API methods for calling server endpoints
+ * This file contains methods for making and processing AJAX calls to the server
  */
 
-const currencies = {
-  EURO: 'EU',
-  POUND: 'UK'
-};
-
-const conversions = {
-  EURO: 0.9,
-  POUND: 0.78
-};
-
-/***********************************************************
- * Handles a Currency Conversion request from the Single 
- * Page Application
- * 
- * @param {string} address the address for the HTTP Request
- * @param {string} currency the result currency
+/*****************************
+ * REST API method constants
  */
-function ajaxConversionRequest(address, currency) {
-  //TODO
+const methods = {
+  GET: 'GET',
+  POST: 'POST'
 }
+
+/*****************************
+ * Currency constants
+ * 
+ * @property {string} EU 
+ * corresponds to EURO
+ * @property {string} UK 
+ * corresponds to POUND
+ * @property {string} US 
+ * corresponds to USD
+ */
+const currencies = {
+  EU: 'EURO',
+  UK: 'POUND',
+  US: 'USD'
+};
 
 /***********************************************************
  * Handles an AJAX Reuqest to the Server
  * 
  * @param {string} address the address for the HTTP request
+ * @param {string} methodType the REST API method type
  */
-function ajaxRequest(address) {
+function ajaxRequest(address, methodType, currency) {
   //TODO
 }
 
@@ -48,3 +52,33 @@ function _getRequest() {
   //TODO
 }
 
+/***********************************************************
+ * Handles displaying the result of a conversion to the
+ * Single Page Application
+ * 
+ * @param {string} currency_type the currency type
+ * @param {number} amount the converted amount
+ */
+function _processConversion(currency_type, amount) {
+  //TODO
+}
+
+/***********************************************************
+ * Handles displaying the history of commands for the
+ * Single Page Application
+ * 
+ * @param {Array} stack the stack of commands
+ */
+function _processHistory(stack) {
+  //TODO
+}
+
+/***********************************************************
+ * Handles popping an element off of the stack of commands
+ * issued in the Single Page Application
+ * 
+ * @param {Array} stack the stack of commands
+ */
+function _processPop(stack) {
+  //TODO
+}
