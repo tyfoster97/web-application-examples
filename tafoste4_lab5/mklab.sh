8ca33d4b6f9b8301614be2ae809148148c2e6250
Ty Foster <tyfoster@outlook.com>
2021-04-15 17:26:12 -0700

    Initial commit

---
+++ tafoste4_lab5/mklab.sh
@@ -0,0 +1,3 @@
+pushd ~/git/421/tafoste4_lab4
+zip ../tafoste4_lab4 README.md activity1/* activity2/activity2.js activity2/utils/* activity2/index.html activity3/index.html activity3/utils/* activity3/activity3.js
+pushd -0
