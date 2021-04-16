1c89303bbda40e9fbbcc026fb1aa0f3dd07b8564
Ty Foster <tyfoster@outlook.com>
2021-04-15 19:18:33 -0700

    Initial commit

---
+++ tafoste4_lab5/activity1/public/javascripts/scripts.js
@@ -0,0 +1,25 @@
+/*******************************************************************************
+ * @file scripts.js
+ * @version 2021.04.15
+ * @author Ty Foster
+ * SER 421, Arizona State University
+ * Copyright 2021, All rights reserved.
+ * 
+ * This file contains methods for the Single Page Application
+ */
+
+/***********************************************************
+ * Handles adding event handlers to DOM Elements as well as
+ * other page setup
+ */
+function setup() {
+  //TODO
+}
+
+/***********************************************************
+ * Checks if the amount entered in the input field is a
+ * valid input, displays an alert if the input is invalid
+ */
+function checkAmount() {
+  //TODO
+}
