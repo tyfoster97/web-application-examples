05803d791d5aa206a3f8f727bb37ead11c7b9bf8
Ty Foster <tyfoster@outlook.com>
2021-04-16 15:59:01 -0700

    Impelments conversion response
    TODO implement stack push

--- tafoste4_lab5/activity1/routes/pound.js
+++ tafoste4_lab5/activity1/routes/pound.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
+const conversion = 0.78;
 
 /* POST conversion. */
 router.post('/', function(req, res, next) {
-  res.send('respond with a resource');
+  const amt = req.query.amount * conversion;
+  //TODO push op to stack
+  const msg = JSON.stringify(
+    {
+      op: 'pound',
+      currency: 'POUND',
+      amount: amt,
+      stack: [] //FIX get stack
+    });
+  res.header('Content-Type', 'application/json; charset=utf-8');
+  res.send(msg);
 });
 
 module.exports = router;
