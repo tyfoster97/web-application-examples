4ef21831c14ee571a187713afdbbe5550fa8aea8
Ty Foster <tyfoster@outlook.com>
2021-04-15 22:45:59 -0700

    Implements initial setup script and helper methods

--- tafoste4_lab5/activity1/public/javascripts/scripts.js
+++ tafoste4_lab5/activity1/public/javascripts/scripts.js
@@ -8,18 +8,53 @@
  * This file contains methods for the Single Page Application
  */
 
+setup(); // handle page set up
+
 /***********************************************************
  * Handles adding event handlers to DOM Elements as well as
  * other page setup
  */
 function setup() {
-  //TODO
+  _disableBtn('euro_btn', true);
+  _disableBtn('pound_btn', true);
+  // Amount input
+  const amt_in = document.getElementById('amount');
+  amt_in.addEventListener('input', function () {
+    _disableBtn('euro_btn', false);
+    _disableBtn('pound_btn', false);
+  });
+  // Euro button
+  const eu_btn = document.getElementById('euro_btn');
+  eu_btn.addEventListener('click', function () {
+    ajaxRequest('/euro', methods.POST, currencies.EU);
+  });
+  // Pound button
+  const uk_btn = document.getElementById('pound_btn');
+  uk_btn.addEventListener('click', function () {
+    ajaxRequest('/pound', methods.POST, currencies.UK);
+  });
+  // Pop button
+  const pop_btn = document.getElementById('pop_btn');
+  pop_btn.addEventListener('click', function () {
+    ajaxRequest('/pop', methods.GET, currencies.EU);
+  });
+  // Reset button
+  const reset_btn = document.getElementById('reset_btn');
+  reset_btn.disabled = true; //FIX enable when stack has elements
+  reset_btn.addEventListener('click', function () {
+    ajaxRequest('/reset', methods.GET, currencies.EU);
+  });
+  // History button
+  const history_btn = document.getElementById('history_btn');
+  history_btn.addEventListener('click', function () {
+    ajaxRequest('/history', methods.GET, currencies.EU);
+  });
 }
 
 /***********************************************************
  * Checks if the amount entered in the input field is a
  * valid input, displays an alert if the input is invalid
  */
-function checkAmount() {
-  //TODO
+function _disableBtn(id, disabled) {
+  document.getElementById(id).disabled = disabled;
 }
