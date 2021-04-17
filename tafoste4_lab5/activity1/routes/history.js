1993913fb1272741bcf2278071d887e5bdee9113
Ty Foster <tyfoster@outlook.com>
2021-04-16 23:00:49 -0700

    Implements history route

--- tafoste4_lab5/activity1/routes/history.js
+++ tafoste4_lab5/activity1/routes/history.js
@@ -1,9 +1,19 @@
 var express = require('express');
+const { connect, close } = require('../utils/db');
+const { Stack } = require('../utils/stack');
 var router = express.Router();
 
-/* GET history of conversions. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+/* GET history of commands */
+router.get('/', async function(req, res, next) {
+  await connect();
+  const s = (await Stack.findOne()).toJSON();
+  await close();
+  const msg = JSON.stringify({
+    op: 'history',
+    stack: s.stack
+  });
+  res.header('Content-Type', 'application/json; charset=utf-8');
+  res.send(msg);
 });
 
 module.exports = router;
