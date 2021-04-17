53bcf57233eff373afe90b711719133c7c6e8ddb
Ty Foster <tyfoster@outlook.com>
2021-04-16 23:38:11 -0700

    Implements pop route

--- tafoste4_lab5/activity1/routes/pop.js
+++ tafoste4_lab5/activity1/routes/pop.js
@@ -1,9 +1,21 @@
 var express = require('express');
+const { connect, close } = require('../utils/db');
+const { Stack } = require('../utils/stack');
 var router = express.Router();
 
 /* GET previous conversion. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', async function(req, res, next) {
+  await connect();
+  const s = await Stack.findOne();
+  const popped = await s.pop();
+  const msg = JSON.stringify({
+    op: 'pop',
+    popped: popped,
+    stack: (await Stack.findOne()).toJSON().stack
+  });
+  await close();
+  res.header('Content-Type', 'application/json; charset=utf-8');
+  res.send(msg);
 });
 
 module.exports = router;
