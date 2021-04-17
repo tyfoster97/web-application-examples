1e3136f2093c400f774d770c4e336e3482d1e4fb
Ty Foster <tyfoster@outlook.com>
2021-04-16 20:54:49 -0700

    Initial commit

---
+++ tafoste4_lab5/activity1/__test__/operation.test.js
@@ -0,0 +1,7 @@
+/**
+ * @file operation.test.js
+ * @version 2021.04.16
+ * @author Ty Foster
+ *
+ * Tests implementation of operation.js
+ */
---
+++ tafoste4_lab5/activity1/utils/operation.js
@@ -0,0 +1,7 @@
+/**
+ * @file operation.js
+ * @version 2021.04.16
+ * @author Ty Foster
+ *
+ * File containing methods for interacting with a user operation
+ */
