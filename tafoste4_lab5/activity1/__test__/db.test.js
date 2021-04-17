458b6309c26fd80393a1a89e37ddba76e04c9648
Ty Foster <tyfoster@outlook.com>
2021-04-16 17:57:08 -0700

    Implements database connection, clean, and close method

---
+++ tafoste4_lab5/activity1/__test__/db.test.js
@@ -0,0 +1,19 @@
+const { connection } = require("mongoose");
+const { connect } = require("../utils/db");
+/*******************************************************************************
+ * @file db.test.js
+ * @version 2021.04.16
+ * @author Ty Foster
+ *
+ * Tests db.js implementation
+ */
+
+describe('Connect to database', () => {
+  test('no parameters passed', async () => {
+    await connect();
+    expect(connection.db.databaseName).toEqual('test');
+    expect(connection.host).toEqual('localhost');
+    expect(connection.port).toEqual(27017);
+    await close();
+  });
+});
---
+++ tafoste4_lab5/activity1/utils/db.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose');
+/*******************************************************************************
+ * @file db.js
+ * @version 2021.04.16
+ * @author Ty Foster
+ * SER 421, Arizona State University
+ * Copyright 2021, all rights reserved.
+ *
+ * Contains functions for interacting with a database
+ */
+
+/***********************************************************
+ * Connects to the specified database
+ *
+ * @param {string} path the uri to the database
+ * @param {string} name the name of the database
+ */
+async function connect(path, name) {
+  const options = {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+  };
+  path = path || 'mongodb://localhost:27017';
+  name = name || 'test';
+  uri = `${path}/${name}?retryWrites=true&w=majority`;
+
+  await mongoose.connect(uri, options);
+}
+
+/***********************************************************
+ * Closes the default connection
+ */
+async function close() {
+  await mongoose.connection.dropDatabase();
+  await mongoose.connection.close();
+};
+
+/***********************************************************
+ * Cleans the default connection
+ */
+async function clean() {
+  for (const key in mongoose.connection.collections) {
+    await mongoose.connection.collections[key].deleteMany({});
+  }
+}
+
+module.exports = {
+  connect,
+  clean,
+  close
+};
