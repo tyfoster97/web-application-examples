6a4af1d85ae0ddb31e1d83631fd3f317c4730479
Ty Foster <tyfoster@outlook.com>
2021-04-16 22:29:18 -0700

    Implements helper method for pushing to stack

--- tafoste4_lab5/activity1/utils/operation.js
+++ tafoste4_lab5/activity1/utils/operation.js
@@ -1,3 +1,5 @@
+const { connect } = require('./db');
+const { Stack } = require('./stack');
 /*******************************************************************************
  * @file operation.js
  * @version 2021.04.16
@@ -28,8 +30,8 @@ class Operation {
    * the operation
    */
   constructor(operand, result, agent, ip) {
-    this.operand = operand || {amount: 0, type: 'USD'};
-    this.result = result || {amount: 0, type: 'USD'};
+    this.operand = operand || { amount: 0, type: 'USD' };
+    this.result = result || { amount: 0, type: 'USD' };
     this.agent = agent || 'unknown';
     this.ip = ip || 'unknown';
   }
@@ -44,6 +46,28 @@ class Operation {
   }
 }
 
+/***********************************************************
+ * Creates a new conversion object and pushes it to the
+ * stack
+ *
+ * @param {number} in_amt the input amount for the
+ * conversion
+ * @param {number} out_amt the amount returned
+ * @param {string} out_currency the type of currency
+ * returned
+ * @param {string} agent the agent requesting the operation
+ * @param {string} ip the ip address of the user requesting
+ * the operation
+ */
+async function pushToStack(in_amt, out_amt, out_currency, agent, ip) {
+  const operand = new Currency(in_amt, 'USD');
+  const result = new Currency(out_amt, out_currency);
+  const op = new Operation(operand, result, agent, ip);
+  const s = (await Stack.findOne()); // one stack element in database
+  await s.push(op);
+}
+
 module.exports = {
- Operation
+  Operation,
+  pushToStack
 };
