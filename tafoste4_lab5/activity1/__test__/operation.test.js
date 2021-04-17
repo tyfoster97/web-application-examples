4d3a3504df13766d5c3be0e2fec86f278ced78a0
Ty Foster <tyfoster@outlook.com>
2021-04-16 21:20:53 -0700

    Implements Operation Class and methods

--- tafoste4_lab5/activity1/__test__/operation.test.js
+++ tafoste4_lab5/activity1/__test__/operation.test.js
@@ -1,7 +1,40 @@
-/**
+const { Operation } = require('../utils/operation');
+/*******************************************************************************
  * @file operation.test.js
  * @version 2021.04.16
  * @author Ty Foster
  *
  * Tests implementation of operation.js
  */
+
+describe('Constructor', () => {
+  test('null parameters', () => {
+    const op = new Operation();
+    expect(op.operand).toEqual({amount: 0, type: 'USD'});
+    expect(op.result).toEqual({amount: 0, type: 'USD'});
+    expect(op.agent).toEqual('unknown');
+    expect(op.ip).toEqual('unknown');
+  });
+
+  test('with parameters', () => {
+    const op = new Operation(
+      {amount: 1, type: 'USD'},
+      {amount: 0.78, type: 'POUND'},
+      'agent',
+      'localhost'
+    );
+    expect(op.operand).toEqual({amount: 1, type: 'USD'});
+    expect(op.result).toEqual({amount: 0.78, type: 'POUND'});
+    expect(op.agent).toEqual('agent');
+    expect(op.ip).toEqual('localhost');
+  });
+});
+
+describe('To String', () => {
+  test('null parameters', () => {
+    const op = new Operation();
+    expect(Operation.toString(op)).toEqual(
+      'Operand: 0 was converted from USD to 0 USD, IP: unknown, User-Details: unknown'
+    );
+  });
+});
--- tafoste4_lab5/activity1/utils/operation.js
+++ tafoste4_lab5/activity1/utils/operation.js
@@ -1,7 +1,49 @@
-/**
+/*******************************************************************************
  * @file operation.js
  * @version 2021.04.16
  * @author Ty Foster
  *
  * File containing methods for interacting with a user operation
  */
+class Currency {
+  /***********************************************************
+   * CONSTRUCTOR
+   * @param {number} amount the amount of the currency
+   * @param {string} type the currency type
+   */
+  /* istanbul ignore next */
+  constructor(amount, type) {
+    this.amount = amount;
+    this.type = type;
+  }
+}
+
+class Operation {
+  /***********************************************************
+   * CONSTRUCTOR
+   * @param {Currency} operand the input for the operation
+   * @param {Currency} result the result of the operation
+   * @param {string} agent the agent requesting the operation
+   * @param {string} ip the ip address of the user performing
+   * the operation
+   */
+  constructor(operand, result, agent, ip) {
+    this.operand = operand || {amount: 0, type: 'USD'};
+    this.result = result || {amount: 0, type: 'USD'};
+    this.agent = agent || 'unknown';
+    this.ip = ip || 'unknown';
+  }
+
+  /***********************************************************
+   * Converts an Operation to a String
+   *
+   * @param {Operation} op the operation object to convert
+   */
+  static toString(op) {
+    return `Operand: ${op.operand.amount} was converted from ${op.operand.type} to ${op.result.amount} ${op.result.type}, IP: ${op.ip}, User-Details: ${op.agent}`;
+  }
+}
+
+module.exports = {
+ Operation
+};
