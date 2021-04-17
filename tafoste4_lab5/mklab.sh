e5179763a86a92993c9f336c89565e2456850a53
Ty Foster <tyfoster@outlook.com>
2021-04-17 00:10:17 -0700

    Adds Activity1 files to script

--- tafoste4_lab5/mklab.sh
+++ tafoste4_lab5/mklab.sh
@@ -1,3 +1,3 @@
-pushd ~/git/421/tafoste4_lab4
-zip ../tafoste4_lab4 README.md activity1/* activity2/activity2.js activity2/utils/* activity2/index.html activity3/index.html activity3/utils/* activity3/activity3.js
+pushd ~/git/421/tafoste4_lab5
+zip ../tafoste4_lab5.zip README.md activity1/__test__/* activity1/bin/www activity1/public/* activity1/routes/* activity1/utils/* activity1/views/* activity1/package.json activity1/server.js
 pushd -0
