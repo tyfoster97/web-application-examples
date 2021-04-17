0bd048c2bf677672f774de6ecf45c8b4e063e1af
Ty Foster <tyfoster@outlook.com>
2021-04-16 23:50:46 -0700

    Sets reset button enabled/disabled on load

--- tafoste4_lab5/activity1/routes/index.js
+++ tafoste4_lab5/activity1/routes/index.js
@@ -1,10 +1,16 @@
 var express = require('express');
 const path = require('path');
+const { connect, close } = require('../utils/db');
+const { Stack } = require('../utils/stack');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index');
+router.get('/', async function(req, res, next) {
+  await connect();
+  const s = await Stack.findOne();
+  const canReset = await s.canClear();
+  await close();
+  res.render('index', {disabled: (!canReset)});
 });
 
 module.exports = router;
