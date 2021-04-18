bc30caa8595c3269d88552539cf19145e083b507
Ty Foster <tyfoster@outlook.com>
2021-04-18 13:36:30 -0700

    Add http error handler

---
+++ tafoste4_lab5/activity1/utils/util.js
@@ -0,0 +1,32 @@
+const createError = require("http-errors");
+/**
+ * @author Ty Foster
+ * @version 2021.04.01
+ * SER 421, Arizona State University
+ * Copyright 2021, all rights reserved.
+ *
+ * Contains generic utility functions
+ */
+
+/**
+ * Renders HTTP Error page for user
+ *
+ * @param {number} code the HTTP error code to send
+ * @param {Request} req the HTTP Request from the client
+ * @param {Response} res the HTTP Response from the server
+ * @param {import("express").NextFunction} next the NextFunction callback for express
+ */
+function httpErr(code, req, res, next) {
+  let err = createError(code);
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+}
+
+module.exports = {
+  httpErr
+};
