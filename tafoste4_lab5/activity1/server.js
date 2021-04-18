bd3d54e136eac56856ddbfbe014b3d2c0b4c0395
Ty Foster <tyfoster@outlook.com>
2021-04-18 13:36:45 -0700

    Add http error handler

--- tafoste4_lab5/activity1/server.js
+++ tafoste4_lab5/activity1/server.js
@@ -11,6 +11,7 @@ var historyRouter = require('./routes/history');
 var popRouter = require('./routes/pop');
 var poundRouter = require('./routes/pound');
 var resetRouter = require('./routes/reset');
+const { httpErr } = require('./utils/util');
 
 var app = express();
 
@@ -33,20 +34,9 @@ app.use('/pound', poundRouter);
 app.use('/reset', resetRouter);
 
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+app.use(function(req, res, next) {
+  httpErr(404, req, res, next);
 });
 
 module.exports = app;
