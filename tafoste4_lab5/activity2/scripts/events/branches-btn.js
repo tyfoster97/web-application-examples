0a6b1faf3baa2ba0507b9ca12bbd8e71d4f7f93a
Ty Foster <tyfoster@outlook.com>
2021-04-18 18:06:54 -0700

    Adds event handlers

---
+++ tafoste4_lab5/activity2/scripts/events/branches-btn.js
@@ -0,0 +1,18 @@
+/*******************************************************************************
+ * @file branches-btn.js
+ * @version 2021.04.18
+ * @author Ty Foster
+ * SER 421, Arizona State University
+ * Copyright 2021, All rights reserved.
+ *
+ * This file contains methods to handle the refresh button being pressed
+ */
+
+/**
+ * Handles the Branches button being clicked
+ *
+ * @param {string} repoName repository name
+ */
+function handleBranchesBtn(repoName) {
+  setupBranchTable(repoName);
+}
