6103e52df877679a7c27ae98f232d8977e4b3c01
Ty Foster <tyfoster@outlook.com>
2021-04-18 18:07:23 -0700

    Implements issue messages being displayed

--- tafoste4_lab5/activity2/scripts/setup/page.js
+++ tafoste4_lab5/activity2/scripts/setup/page.js
@@ -18,3 +18,27 @@ function setupPg() {
     handleUnameBtn();
   });
 }
+
+/**
+ * Determines the average open issues and the
+ * maximum issue repo, displays on webpage
+ *
+ * @param {Array<Object>} data
+ */
+function _issueMsgs(data) {
+  let sum = 0;
+  let count = 0;
+  let max = 0;
+  let maxR = '';
+  for (const repo of data) {
+    sum += repo.open_issues_count;
+    count++;
+    if (repo.open_issues_count > max) {
+      max = repo.open_issues_count;
+      maxR = repo.name;
+    }
+  }
+  const avg = sum / count;
+
+  document.getElementById('issues').innerHTML = `The average number of issues is ${avg} and the repository with the maximum number of issues is ${maxR}`;
+}
