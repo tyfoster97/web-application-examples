22434dc6ab87e250d6662551f9edd4031e7a890e
Ty Foster <tyfoster@outlook.com>
2021-04-22 12:28:10 -0700

    Renders InfoPanel

--- tafoste4_lab6/test/components/InfoPanel.test.jsx
+++ tafoste4_lab6/test/components/InfoPanel.test.jsx
@@ -7,4 +7,16 @@ describe('<InfoPanel />', () => {
     render(<InfoPanel />);
     expect(screen.getByRole('infopanel')).toBeInTheDocument();
   });
+
+  it('should render given stats', () => {
+    const stats = [
+      {foo: 'bar'}
+    ];
+    render(<InfoPanel stats={stats} />);
+    // verify KeyValueList rendered
+    expect(screen.getByRole('wrapper')).toBeInTheDocument();
+    expect(screen.getByRole('pairwrapper')).toBeInTheDocument();
+    expect(screen.getByRole('key')).toBeInTheDocument();
+    expect(screen.getByRole('value')).toBeInTheDocument();
+  });
 });
