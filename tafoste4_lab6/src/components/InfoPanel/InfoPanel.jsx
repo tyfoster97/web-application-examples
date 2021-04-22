1690d5e150e1bba92507f4f191658d4c8d6a71be
Ty Foster <tyfoster@outlook.com>
2021-04-22 12:24:23 -0700

    Renders InfoPanel

---
+++ tafoste4_lab6/src/components/InfoPanel/InfoPanel.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled from 'styled-components';
+import { KeyValueList } from '../index';
+
+export const InfoPanel = () => {
+  return <StatsWrapper role='infopanel' />;
+}
+
+const StatsWrapper = styled.div``;
+
+const NoInfoWrapper = styled.div``;
+
+const NoInfo = styled.div``;
--- tafoste4_lab6/src/components/index.js
+++ tafoste4_lab6/src/components/index.js
@@ -1,6 +1,7 @@
 export { Button } from './Button/Button';
 export { Checkbox } from './Checkbox/Checkbox';
 export { Image } from './Image/Image';
+export { InfoPanel } from './InfoPanel/InfoPanel';
 export { KeyValueList } from './KeyValueList/KeyValueList';
 export { SelectField } from './SelectField/SelectField';
 export { TextField } from './TextField/TextField';
