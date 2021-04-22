a5a2deabe9113e2d95751e08dc239b8938819f69
Ty Foster <tyfoster@outlook.com>
2021-04-21 18:11:41 -0700

    Adds styling

--- tafoste4_lab6/src/components/Button/Button.jsx
+++ tafoste4_lab6/src/components/Button/Button.jsx
@@ -2,14 +2,42 @@ import React from 'react';
 import styled from 'styled-components';
 
 export const Button = (props) => {
-  return <button
+  return <Btn
       value={props.value}
       onClick={(e) => props.onClick && props.onClick(e.target)}
-    >
-  </button>;
+    />;
 };
 
 Button.defaultProps = {
   value: "",
   onClick: ()=>{}
 };
+
+const Btn = styled.button`
+  height: 50px;
+  border-radius: 5px;
+  font-size: 16px;
+  outline: 0;
+  padding: 0 50px;
+  border: none;
+  color: #fff;
+  box-sizing: border-box;
+  background-color: #3498db;
+  cursor: pointer;
+  text-decoration: none;
+  text-align: center;
+  font-family: 'Open Sans',sans-serif;
+  font-weight: 700;
+
+  &:focus {
+    outline: 0;
+    color: #fff;
+  }
+  &:hover {
+    background: linear-gradient(0deg, rgba(52,152,219,1) 0%, rgba(52,152,219,1) 35%, rgba(125,183,222,1) 100%);
+  }
+  &:active {
+    box-shadow: inset 2px 3px 6px -2px black;
+    background: #2e85c0;
+  }
+`;
--- tafoste4_lab6/test/components/Button.test.jsx
+++ tafoste4_lab6/test/components/Button.test.jsx
@@ -20,4 +20,24 @@ describe('<Button />', () => {
     act(() => userEvent.click(screen.getByRole('button')));
     expect(cb).toHaveBeenCalled();
   });
+
+  it('should render with style', () => {
+    render(<Button />);
+    expect(screen.getByRole('button')).toHaveStyle(`
+      height: 50px;
+      border-radius: 5px;
+      font-size: 16px;
+      outline: 0;
+      padding: 0 50px;
+      border: none;
+      color: #fff;
+      box-sizing: border-box;
+      background-color: #3498db;
+      cursor: pointer;
+      text-decoration: none;
+      text-align: center;
+      font-family: 'Open Sans',sans-serif;
+      font-weight: 700;
+    `);
+  })
 });
