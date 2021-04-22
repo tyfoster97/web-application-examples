a6120661977670a49bd99673bfff0cdcadab6013
Ty Foster <tyfoster@outlook.com>
2021-04-22 12:41:39 -0700

    Implements InfoPanel

--- tafoste4_lab6/src/components/InfoPanel/InfoPanel.jsx
+++ tafoste4_lab6/src/components/InfoPanel/InfoPanel.jsx
@@ -2,12 +2,36 @@ import React from 'react';
 import styled from 'styled-components';
 import { KeyValueList } from '../index';
 
-export const InfoPanel = () => {
-  return <StatsWrapper role='infopanel' />;
+export const InfoPanel = (props) => {
+  const child = (props.stats) ? <KeyValueList items={props.stats} /> : <NoInfoWrapper role='wrapper' children={<NoInfo role='noinfo'>No Info</NoInfo>}/>;
+  return <StatsWrapper
+    role='infopanel'
+    stats={props.stats}
+    children={child}
+  />;
 }
 
-const StatsWrapper = styled.div``;
+InfoPanel.defaultProps = {
+  stats: null
+};
 
-const NoInfoWrapper = styled.div``;
+const StatsWrapper = styled.div`
+  padding-left: 40px;
+  height: 50%;
+  justify-content: flex-end;
+  display: flex;
+`;
 
-const NoInfo = styled.div``;
+const NoInfoWrapper = styled.div`
+  height: 52%;
+  width: 98%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const NoInfo = styled.div`
+  color: #555;
+  font-weight: 700;
+  font-family: Open Sans,sans-serif;
+`;
--- tafoste4_lab6/test/components/InfoPanel.test.jsx
+++ tafoste4_lab6/test/components/InfoPanel.test.jsx
@@ -21,4 +21,33 @@ describe('<InfoPanel />', () => {
     expect(screen.getByText('foo')).toBeInTheDocument();
     expect(screen.getByText('bar')).toBeInTheDocument();
   });
+
+  it('should render no info', () => {
+    render(<InfoPanel />);
+    expect(screen.getByRole('wrapper')).toBeInTheDocument();
+    expect(screen.getByRole('noinfo')).toBeInTheDocument();
+    expect(screen.getByText('No Info')).toBeInTheDocument();
+  });
+
+  it('should render with style', () => {
+    render(<InfoPanel />);
+    expect(screen.getByRole('infopanel')).toHaveStyle(`
+      padding-left: 40px;
+      height: 50%;
+      justify-content: flex-end;
+      display: flex;
+    `);
+    expect(screen.getByRole('wrapper')).toHaveStyle(`
+      height: 52%;
+      width: 98%;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+    `);
+    expect(screen.getByRole('noinfo')).toHaveStyle(`
+      color: #555;
+      font-weight: 700;
+      font-family: Open Sans,sans-serif;
+    `);
+  });
 });
